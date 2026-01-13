bb57247b8c1692090f856b6f35021ececd58f702
Saim <saimhasan514@gmail.com>
2026-01-13 08:32:15 +0600

    initial grid and display

--- lightsOut.c
+++ lightsOut.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+#define SIZE 5
+
 int main()
 {
-    printf("Lights Out Game\n");
+    int grid[SIZE][SIZE];
+
+    for (int i=0; i<SIZE; i++)
+    {
+        for(int j=0; j < SIZE; j++)
+            grid[i][j] = 0;
+    }
+
+    for (int i=0; i<SIZE; i++)
+    {
+        printf("\n");
+        for(int j=0; j < SIZE; j++)
+            printf("%d ", grid[i][j]);
+    }
+
+    printf("\nLights Out Game\n");
     return 0;
 }
