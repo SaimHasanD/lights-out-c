093055527d827cebfb84136a5bd353474024c86b
Saim <saimhasan514@gmail.com>
2026-01-13 19:26:52 +0600

    Win checker

--- lightsOut.c
+++ lightsOut.c
@@ -18,11 +18,12 @@ int main()
 
     srand(time(NULL));
 
-    for (int k = 0; k < 5; k ++)
+    for (int k = 0; k < 2; k ++)
     {
         row = rand() % SIZE;
         col = rand() % SIZE;
 
+        //center
         grid[row][col] = 1 - grid[row][col];
         // up
         if (row > 0)
@@ -39,14 +40,21 @@ int main()
         // right
         if (col < SIZE - 1)
         grid[row][col + 1] = 1 - grid[row][col + 1];
+
     }
 
 
     while(1)
         {
+            printf("    ");
+            for (int i = 0; i < SIZE; i++)
+                printf("%d ", i);
+            printf("\n");
+            printf("   -----------");
+
             for (int i=0; i<SIZE; i++)
             {
-                printf("\n");
+                printf("\n%d | ", i);
                 for(int j=0; j < SIZE; j++)
                     if (grid[i][j] == 0)
                         printf(". ");
@@ -79,11 +87,22 @@ int main()
                         grid[row][col + 1] = 1 - grid[row][col + 1];
                 }
 
+            int won = 1;
+            for (int i=0; i<SIZE; i++)
+                {
+                for(int j=0; j < SIZE; j++)
+                 {
+                     if(grid[i][j] == 1)
+                        won = 0;
+                 }
+                }
+            if (won)
+            {
+                printf("\nYou win");
+                break;
+            }
+
         }
 
-
-
-
-    printf("\nLights Out Game\n");
     return 0;
 }
