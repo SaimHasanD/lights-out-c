cf02c992afeb66c96dd031cfee3d30aa4e907fd2
Saim <saimhasan514@gmail.com>
2026-01-13 08:51:58 +0600

    cell flip

--- lightsOut.c
+++ lightsOut.c
@@ -6,22 +6,35 @@ int main()
 {
     int grid[SIZE][SIZE];
 
+    int row, col;
+
     for (int i=0; i<SIZE; i++)
     {
         for(int j=0; j < SIZE; j++)
             grid[i][j] = 0;
     }
 
+    printf("Enter a row and column: ");
+    scanf("%d %d", &row,&col);
+
+    if (grid[row][col] == 0)
+                grid[row][col] = 1;
+            else
+                grid[row][col] = 0;
+
+
     for (int i=0; i<SIZE; i++)
     {
         printf("\n");
         for(int j=0; j < SIZE; j++)
             if (grid[i][j] == 0)
-            printf(". ");
-        else
-            printf("* ");
+                printf(". ");
+            else
+                printf("* ");
 
     }
+
+
 
     printf("\nLights Out Game\n");
     return 0;
