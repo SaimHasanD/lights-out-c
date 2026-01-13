f43071f60685cea46a0e2a6eee93e0d9655cf6ff
Saim <saimhasan514@gmail.com>
2026-01-13 09:47:48 +0600

    games on loop

--- lightsOut.c
+++ lightsOut.c
@@ -14,42 +14,45 @@ int main()
             grid[i][j] = 0;
     }
 
-    printf("Enter a row and column: ");
-    scanf("%d %d", &row,&col);
-
-    if (row >= SIZE || col >= SIZE || row < 0 || col < 0)
-        printf("Enter a correct value");
-
-    else
+    while(1)
         {
-            grid[row][col] = 1 - grid[row][col];
-            // up
-            if (row > 0)
-                grid[row - 1][col] = 1 - grid[row - 1][col];
+            for (int i=0; i<SIZE; i++)
+            {
+                printf("\n");
+                for(int j=0; j < SIZE; j++)
+                    if (grid[i][j] == 0)
+                        printf(". ");
+                    else
+                        printf("* ");
+            }
+            printf("\n\nEnter a row and column: ");
+            scanf("%d %d", &row,&col);
 
-            // down
-            if (row < SIZE - 1)
-                grid[row + 1][col] = 1 - grid[row + 1][col];
+            if (row >= SIZE || col >= SIZE || row < 0 || col < 0)
+                printf("Enter a correct value");
 
-            // left
-            if (col > 0)
-                grid[row][col - 1] = 1 - grid[row][col - 1];
+            else
+                {
+                    grid[row][col] = 1 - grid[row][col];
+                    // up
+                    if (row > 0)
+                        grid[row - 1][col] = 1 - grid[row - 1][col];
 
-            // right
-            if (col < SIZE - 1)
-                grid[row][col + 1] = 1 - grid[row][col + 1];
+                    // down
+                    if (row < SIZE - 1)
+                        grid[row + 1][col] = 1 - grid[row + 1][col];
+
+                    // left
+                    if (col > 0)
+                        grid[row][col - 1] = 1 - grid[row][col - 1];
+
+                    // right
+                    if (col < SIZE - 1)
+                        grid[row][col + 1] = 1 - grid[row][col + 1];
+                }
+
         }
 
-    for (int i=0; i<SIZE; i++)
-    {
-        printf("\n");
-        for(int j=0; j < SIZE; j++)
-            if (grid[i][j] == 0)
-                printf(". ");
-            else
-                printf("* ");
-
-    }
 
 
 
